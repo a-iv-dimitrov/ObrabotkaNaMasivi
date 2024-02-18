f06481f596387e87870f2982960148550b35a926
a-iv-dimitrov <alivdim@gmail.com>
2024-02-18 11:33:35 +0200

    Add project files.

---
+++ BezopasnaObrabtokaMasivi/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Runtime.InteropServices;
+
+namespace BezopasnaObrabtokaMasivi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "END")
+                {
+                    break;
+                }
+
+                string[] commandArgs = command.Split();
+
+                switch (commandArgs[0])
+                {
+                    case "Reverse":
+                        Array.Reverse(array);
+                        break;
+                    case "Distinct":
+                        array = array.Distinct().ToArray();
+                        break;
+                    case "Replace":
+                        int index;
+                        if (!int.TryParse(commandArgs[1], out index))
+                        {
+                            Console.WriteLine("Невалиден вход!");
+                            continue;
+                        }
+
+                        if (index < 0 || index >= array.Length)
+                        {
+                            Console.WriteLine("Невалиден вход!");
+                            continue;
+                        }
+
+                        string str = commandArgs[2];
+                        array[index] = str;
+                        break;
+                    default:
+                        Console.WriteLine("Невалидна команда!");
+                        break;
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
---
+++ ObrabotkaNaMasivi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Runtime.ExceptionServices;
+
+namespace ObrabotkaNaMasivi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++) 
+            {
+                string[] input = Console.ReadLine().Split();
+
+                switch(input[0])
+                {
+                    case "Reverse":
+                        Array.Reverse(array); break;
+                    case "Distinct":
+                        array = array.Distinct().ToArray(); break;
+                    case "Replace":
+                        int index = int.Parse(input[1]);
+                        string str = input[2];
+                        array[index] = str;
+                        break;
+                }
+            }
+            Console.WriteLine(string.Join(", ", array));
+        }
+    }
+}
